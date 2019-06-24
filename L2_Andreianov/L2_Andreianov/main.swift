59aab3c9eafc5f982e5f0eeec428404097f57164
domenada <adventurersuper@gmail.com>
2019-06-24 12:09:56 -0400

    4 задания

--- L2_Andreianov/L2_Andreianov/main.swift
+++ L2_Andreianov/L2_Andreianov/main.swift
@@ -8,5 +8,53 @@
 
 import Foundation
 
-print("Hello, World!")
+
+//Задание 1
+
+func evenNumbers (Array: Int) -> [Int] {
+    let Array = [3,4,5,7,8]
+    for  Element in Array {
+        if Element % 2 == 0 {
+            print("это четное число")
+        } else {
+            print("это нечетное число")
+        }
+    }
+    return Array
+}
+
+//Задание 2
+
+func threeNumbers (Array: Int) -> [Int] {
+    let Array = [3,4,5,7,8]
+    for  Element in Array {
+        if Element % 3 == 0 {
+            print("это число делится на 3")
+        } else {
+            print("это число не делится на 3")
+        }
+    }
+    return Array
+}
+
+//Задание 3
+
+var newArray: [Int] = []
+for i in 0...100 {
+    newArray.append(i)
+}
+print (newArray)
+
+
+// Задание 4
+
+for value in newArray {
+    if (value % 2) == 0 || (value % 3) == 0 {
+        newArray.remove(at: newArray.index(of: value)!)
+    }
+}
+
+print (newArray)
+
+
 
