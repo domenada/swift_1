210420408a357315779e2dbea17cf4bb5a510f7f
domenada <adventurersuper@gmail.com>
2019-06-24 04:47:40 -0400

    Выполнены 4 задания

---
+++ L2_Andreianov/L2_Andreianov/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L2_Andreianov
+//
+//  Created by Dmitry Andreyanov on 6/22/19.
+//  Copyright © 2019 Dmitrii Andreianov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
